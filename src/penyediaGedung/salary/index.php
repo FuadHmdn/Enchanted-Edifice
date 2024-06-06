65acbc0b7953e2256be5c423b2ef88bef9d4a9cd
widyastibella <wdyabella@gmail.com>
2024-06-07 01:04:30 +0700

    salary

--- src/penyediaGedung/salary/index.php
+++ src/penyediaGedung/salary/index.php
@@ -6,67 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Salary Enchanted Edifice</title>
+    <title>Home Enchanted Edifice</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap">
+
 
     <style>
         body {
             background-color: #F3F4F7;
-            font-family: 'Open Sans', sans-serif;
         }
 
-        .navbar {
-            background-color: #fff;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        @font-face {
+            font-family: 'Abril Fatface';
+            src: url('/res/Abril_Fatface/AbrilFatface-Regular.woff2') format('woff2');
+            font-weight: normal;
+            font-style: normal;
         }
 
         .nav-link {
+            font-family: 'Lato', sans-serif;
             font-weight: bold;
             font-size: 16px;
-            color: #0a1e3f;
             margin-right: 20px;
         }
 
-        .nav-link.active {
-            color: #007bff;
+        .bg-home {
+            max-width: 100%;
+            height: auto;
+            padding-bottom: 120px;
+            background-color: #E2E9F8;
         }
 
-        .header-image {
-            position: relative;
-            text-align: center;
-            margin-bottom: 40px;
-            overflow: hidden;
-            border-radius: 15px;
-        }
-
-        .header-image img {
-            width: 100%;
-            max-height: 400px;
-            object-fit: cover;
-            filter: brightness(70%);
-        }
-
-        .header-image .overlay {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.7));
-            opacity: 0.9;
-        }
-
-        .header-image .overlay-text {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            color: #fff;
-            font-size: 48px;
-            font-weight: bold;
-            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
-        }
-
+        
         .team-card {
             background-color: #fff;
             border-radius: 15px;
@@ -103,36 +73,18 @@
             border-radius: 20px;
         }
 
-        .footer {
-            background-color: #fff;
-            padding: 20px 0;
-            text-align: center;
-            box-shadow: 0 -2px 4px rgba(0, 0, 0, 0.1);
-        }
-
-        .footer img {
-            width: 150px;
-            margin-bottom: 10px;
-        }
-
-        .footer p {
-            margin: 5px 0;
-            color: #555;
-        }
-
-        .footer .social-icons img {
-            width: 30px;
-            margin: 0 10px;
-        }
     </style>
+
 </head>
 
 <body>
+
     <!-- NAVIGASI -->
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="position: sticky; top: 0; z-index: 1000;">
         <div class="container-fluid">
             <a class="navbar-brand" href="/home">
-                <img src="../../res/logo_and_name.png" alt="Logo" style="width: 210px; height: auto;">
+                <img src="../../res/logo_and_name.png" style="width: 210px; height: auto; margin-left: 46px;"
+                    alt="Logo">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -140,21 +92,21 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto" style="margin-right: 46px;">
                     <li class="nav-item">
-                        <a class="nav-link" href="../home/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>">HOME</a>
+                        <a class="nav-link active" aria-current="page" href="../home/" style="color: #0a1e3f;">HOME</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../offer/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>">OFFERS</a>
+                        <a class="nav-link" href="../offer/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">OFFERS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../order/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>">ORDERS</a>
+                        <a class="nav-link" href="../order/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">ORDERS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../review/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>">REVIEW</a>
+                        <a class="nav-link" href="../review/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">REVIEW</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="../salary/">SALARY</a>
+                        <a class="nav-link" href="../salary/?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">SALARY</a>
                     </li>
                     <li class="nav-item">
                         <button onclick="profileClick()" class="btn btn-outline-secondary" style="border-radius: 15px;">
@@ -169,15 +121,19 @@
         </div>
     </nav>
 
-    <!-- HEADER IMAGE -->
-    <div class="header-image">
-        <img src="../salary/image/Salary.jpg" alt="Salary Header Image">
-        <div class="overlay"></div>
-        <div class="overlay-text">Salary</div>
+    <!-- HOME IMAGE -->
+     
+    <div style="margin-left: 46px; margin-right: 46px; width: auto; height: auto; text-align: center; position: relative; margin-top: 30px;">
+        <img src="../salary/image/TFC Funding Process Help You With The Bank Processes.jpeg" alt="Order Image" style="width: 100%; border-radius:40px">
+        <div class="image-text"
+            style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
+            <p style="font-family: 'Lato',sans-serif; font-size: 90px; font-weight: bold; color: #ffffff; display: inline-block;">SALARY</p>
+        </div>
     </div>
 
+    
     <!-- TEAM CARDS -->
-    <div class="container">
+    <div class="container"style="margin-bottom: 90px; margin-top: 80px;">
         <div class="team-card">
             <img src="../salary/image/chief.png" alt="Chief of Company" width="60">
             <div class="team-info">
@@ -224,21 +180,52 @@
         </div>
     </div>
 
-    <!-- FOOTER -->
-    <div class="footer">
-        <div class="container">
-            <img src="../../res/logo_and_name.png" alt="Logo">
-            <p>Enchanting Events, Enchanted Experiences!</p>
-            <div class="social-icons">
+
+    <!-- BOTTOM BAR -->
+    <div
+        style="display: flex; flex-direction: row; padding-right: 46px; padding-left: 46px; justify-content: space-between; padding-top: 30px; padding-bottom: 20px;">
+
+        <div style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
+            <img src="../../res/logo_and_name.png" style="width: 210px; height: auto;" alt="Logo">
+            <p
+                style="margin: 0; padding-left: 50px; font-size: 16px; font-family: 'Roboto', sans-serif; color: #545454; font-weight: bold;">
+                Enchanting
+                Events, Enchanted<br>Experiences!</p>
+        </div>
+
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+            <p
+                style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
+                Services</p>
+            <p style="margin: 0;">Booking</p>
+        </div>
+
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+            <p
+                style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
+                About</p>
+            <p style="margin: 0;">Our Story</p>
+            <p style="margin: 0;">Blog</p>
+        </div>
+
+        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
+            <p
+                style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
+                Follow Us
+            </p>
+
+            <div style="display: flex; flex-direction: row;">
                 <img src="../../res/Facebook.png" alt="Facebook">
                 <img src="../../res/Twitter.png" alt="Twitter">
                 <img src="../../res/LinkedIn.png" alt="LinkedIn">
             </div>
         </div>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
     <script>
         function profileClick() {
@@ -246,5 +233,4 @@
         }
     </script>
 </body>
-
 </html>
