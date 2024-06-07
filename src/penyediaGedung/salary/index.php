0ffc6bd646fbfb58f8b27738662761a4e33727d0
Dimasadivia <Dimas020417@gmail.com>
2024-06-07 09:18:45 +0700

    salary ; revisi

--- src/penyediaGedung/salary/index.php
+++ src/penyediaGedung/salary/index.php
@@ -35,7 +35,6 @@
             background-color: #E2E9F8;
         }
 
-        
         .team-card {
             background-color: #fff;
             border-radius: 15px;
@@ -71,7 +70,6 @@
             padding: 10px;
             border-radius: 20px;
         }
-
     </style>
 
 </head>
@@ -93,7 +91,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto" style="margin-right: 46px;">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="../home/" style="color: #0a1e3f;">HOME</a>
+                        <a class="nav-link" href="../home/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">HOME</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../offer/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">OFFERS</a>
@@ -105,7 +103,7 @@
                         <a class="nav-link" href="../review/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">REVIEW</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../salary/?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #8692A6;">SALARY</a>
+                        <a class="nav-link active" aria-current="page" href="../salary/?id=<?php echo htmlspecialchars($_GET['id']); ?>" style="color: #0a1e3f;">SALARY</a>
                     </li>
                     <li class="nav-item">
                         <button onclick="profileClick()" class="btn btn-outline-secondary" style="border-radius: 15px;">
@@ -121,18 +119,15 @@
     </nav>
 
     <!-- HOME IMAGE -->
-     
     <div style="margin-left: 46px; margin-right: 46px; width: auto; height: auto; text-align: center; position: relative; margin-top: 30px;">
-        <img src="../salary/image/TFC Funding Process Help You With The Bank Processes.jpeg" alt="Order Image" style="width: 100%; border-radius:40px">
-        <div class="image-text"
-            style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
-            <p style="font-family: 'Lato',sans-serif; font-size: 90px; font-weight: bold; color: #ffffff; display: inline-block;">SALARY</p>
+        <img src="../salary/image/TFC Funding Process Help You With The Bank Processes.jpeg" alt="Order Image" style="width: 100%; border-radius: 40px;">
+        <div class="image-text" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
+            <p style="font-family: 'Lato', sans-serif; font-size: 90px; font-weight: bold; color: #ffffff; display: inline-block;">SALARY</p>
         </div>
     </div>
 
-    
     <!-- TEAM CARDS -->
-    <div class="container"style="margin-bottom: 90px; margin-top: 80px;">
+    <div class="container" style="margin-bottom: 90px; margin-top: 80px;">
         <div class="team-card">
             <img src="../salary/image/chief.png" alt="Chief of Company" width="60">
             <div class="team-info">
@@ -179,57 +174,30 @@
         </div>
     </div>
 
-
     <!-- BOTTOM BAR -->
-    <div
-        style="display: flex; flex-direction: row; padding-right: 46px; padding-left: 46px; justify-content: space-between; padding-top: 30px; padding-bottom: 20px;">
-
+    <div style="display: flex; flex-direction: row; padding-right: 46px; padding-left: 46px; justify-content: space-between; padding-top: 30px; padding-bottom: 20px;">
         <div style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
             <img src="../../res/logo_and_name.png" style="width: 210px; height: auto;" alt="Logo">
-            <p
-                style="margin: 0; padding-left: 50px; font-size: 16px; font-family: 'Roboto', sans-serif; color: #545454; font-weight: bold;">
-                Enchanting
-                Events, Enchanted<br>Experiences!</p>
+            <p style="margin: 0; padding-left: 50px; font-size: 16px; font-family: 'Roboto', sans-serif; color: #545454; font-weight: bold;">
+                Enchanting Events, Enchanted<br>Experiences!
+            </p>
         </div>
-
         <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-            <p
-                style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
+            <p style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
                 Services</p>
             <p style="margin: 0;">Booking</p>
         </div>
-
         <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-            <p
-                style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
-                About</p>
-            <p style="margin: 0;">Our Story</p>
-            <p style="margin: 0;">Blog</p>
+            <p style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
+                Contact</p>
+            <p style="margin: 0;">+123456789</p>
         </div>
-
-        <div style="display: flex; flex-direction: column; justify-content: center; align-items: center;">
-            <p
-                style="margin: 0; font-size: 16px; font-family: 'Roboto', sans-serif; color: #8692A6; font-weight: bold;">
-                Follow Us
-            </p>
-
-            <div style="display: flex; flex-direction: row;">
-                <img src="../../res/Facebook.png" alt="Facebook">
-                <img src="../../res/Twitter.png" alt="Twitter">
-                <img src="../../res/LinkedIn.png" alt="LinkedIn">
-            </div>
-        </div>
-
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz4fnFO9gybBogGzOgUicvyh4P4pndK9mWrwOHUUBgGIl3oNsG5sycX/++" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-g16na5YMmYIwKNfDCKTjGU23DCR9U6XTdIO4cIQ2B5D2Rw1lPseLGH3D6/sUF5F+" crossorigin="anonymous"></script>
 
-    <script>
-        function profileClick() {
-            window.location.href = "../profile/index.php?id=<?php echo htmlspecialchars($_GET['id']); ?>";
-        }
-    </script>
 </body>
 </html>
